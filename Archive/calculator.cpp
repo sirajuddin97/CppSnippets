726cab6cb02028755eec978477f369498ad278c6
Sirajuddin Asjad <sirasjad@gmail.com>
2018-01-03 23:44:10 +0100

    Update

---
+++ Archive/calculator.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int a;
+  int b;
+  int sum;
+
+  cout << "Enter a number:\n";
+  cin >> a;
+  cout << "Enter another number:\n";
+  cin >> b;
+  cout << "Sum: " << a+b;
+  return 0;
+}
