5772374f7483ded2aff321f6c4b309d7912ac9f5
Sirajuddin Asjad <sirasjad@gmail.com>
2018-01-19 11:10:17 +0100

    Update

---
+++ Objektorientering, algoritmer og datastrukturer/Lab_2/chap5_drill.cpp
@@ -0,0 +1,23 @@
+#include "../../std_lib_facilities.h"
+
+int main(){
+  try{
+    int res = 7;
+    vector<int> v(10);
+    v[5] = res;
+    cout << "Success!\n";
+
+    char ch; cin >> ch;
+    return 0;
+  }
+  catch(exception& e){
+    cerr << "Error: " << e.what() << '\n';
+    char ch; cin >> ch;
+    return 1;
+  }
+  catch(...){
+    cerr << "Oops: unknown exception!\n";
+    char ch; cin >> ch;
+    return 2;
+  }
+}
