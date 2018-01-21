170ab1f373274826102465a7da08a418a076af89
Sirajuddin Asjad <sirasjad@gmail.com>
2018-01-21 15:51:45 +0100

    Update

--- Objektorientering, algoritmer og datastrukturer/Lab_2/chap5_exercise12.cpp
+++ Objektorientering, algoritmer og datastrukturer/Lab_2/chap5_exercise12.cpp
@@ -15,14 +15,11 @@ int sjekkStr(int tall){
 void startSpill(vector<char> tallVektor){
   system("clear");
   int gjettTall = 0;
-  int riktigTall1 = 0;
-  int riktigTall2 = 0;
-  int riktigTall3 = 0;
-  int riktigTall4 = 0;
-  cout << "Spillet har startet! Skriv -1 for å avslutte spillet." << endl;
+  int riktig = 0, riktigTall1 = 0, riktigTall2 = 0, riktigTall3 = 0, riktigTall4 = 0;
+  cout << "SPILLET HAR STARTET! LYKKE TIL! ;-)" << endl;
 
   while(true){
-    cout << "Gjett et firesifret tall: ";
+    cout << "Gjett et firesifret tall (" << riktig << "/4 riktig så langt): ";
     cin >> gjettTall;
     if(!cin){ throw -1; }
 
@@ -31,22 +28,28 @@ void startSpill(vector<char> tallVektor){
       if(gjett[0] == tallVektor[0] && riktigTall1 == 0){
         cout << "\tTall 1 er riktig!" << endl;
         riktigTall1 = 1;
+        riktig++;
       }
       if(gjett[1] == tallVektor[1] && riktigTall2 == 0){
         cout << "\tTall 2 er riktig!" << endl;
         riktigTall2 = 1;
+        riktig++;
       }
       if(gjett[2] == tallVektor[2] && riktigTall3 == 0){
         cout << "\tTall 3 er riktig!" << endl;
         riktigTall3 = 1;
+        riktig++;
       }
       if(gjett[3] == tallVektor[3] && riktigTall4 == 0){
         cout << "\tTall 4 er riktig!" << endl;
         riktigTall4 = 1;
+        riktig++;
       }
 
       if(riktigTall1 == 1 && riktigTall2 == 1 && riktigTall3 == 1 && riktigTall4 == 1){
+        system("clear");
         cout << "Gratulerer! Du har fullført spillet!" << endl;
+        cout << "Riktig tall var " << tallVektor[0] << tallVektor[1] << tallVektor[2] << tallVektor[3] << "." << endl;
         break;
       }
     }
@@ -72,7 +75,7 @@ void skrivTall(){
     tallVektor.push_back(nyttTall[3]);
 
     cout << "\tFlott! Spillet starter om 5 sekunder..." << endl;
-    usleep(5000000); // 4 seconds
+    usleep(5000000); // 5 seconds
     startSpill(tallVektor);
   }
   else{
