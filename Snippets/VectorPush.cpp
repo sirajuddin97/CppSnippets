e39df2f2a381eed322e70474c3fba576be6beebf
Sirajuddin Asjad <sirasjad@gmail.com>
2018-03-30 22:17:12 +0200

    Update

---
+++ Snippets/VectorPush.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Company{
+public:
+  string name;
+};
+
+int main(){
+  // Uten pekere:
+  Company a;
+  a.name = "Elkjop";
+  vector<Company> v;
+  v.push_back(a);
+
+  // Med pekere (smart metode):
+  Company b;
+  b.name = "Elkjop";
+  Company* c = &b;
+  vector<Company*> v2;
+  v2.push_back(c);
+
+  // Med pekere (ikke smart metode):
+  Company* d = new Company;
+  d->name = "Elkjop";
+  vector<Company*> v3;
+  v3.push_back(d);
+  delete d;
+
+  cout << v[0].name << endl;
+  cout << v2[0]->name << endl;
+  cout << v3[0]->name << endl;
+  return 0;
+}
