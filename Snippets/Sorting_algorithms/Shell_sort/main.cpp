adcdf49aaf4102d6300f56bc720b8ae6427d101e
Sirajuddin Asjad <sirasjad@gmail.com>
2018-04-27 10:55:20 +0200

    Fix

--- Snippets/Sorting_algorithms/Shell_sort/main.cpp
+++ Snippets/Sorting_algorithms/Shell_sort/main.cpp
@@ -5,7 +5,6 @@ using namespace std;
 static const int length = 10;
 
 void print_list(int list[]){
-  cout << "\t";
   for(int i = 0; i < length; i++){
     cout << list[i] << " ";
   }
@@ -18,21 +17,22 @@ void swap_int(int& i1, int& i2){
   i2 = tmp;
 }
 
-void shell_sort(int list[]){
-  int gap = length/2;
-  while(gap > 0){
-    insertion_sort(list, gap);
-    gap = gap/2;
-  }
-}
-
 void insertion_sort(int list[], int gap){
   for(int i = gap; i < length; i++){
     int j = i;
     while((j >= gap) && (list[j] < list[j-gap])){
       swap_int(list[j], list[j-gap]);
+      cout << "\tSwapped " << list[j] << " with " << list[j-gap] << endl;
       j = j-gap;
     }
+  }
+}
+
+void shell_sort(int list[]){
+  int gap = length/2;
+  while(gap > 0){
+    insertion_sort(list, gap);
+    gap = gap/2;
   }
 }
 
